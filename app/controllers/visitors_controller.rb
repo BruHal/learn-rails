41d302e23f177a069f4daacbb6e97692b8236821
Bruce Halvorson <bruce.halvorson@oracle.com>
2015-03-26 10:32:26 -0600

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome Butt nugget!'
+    flash.now[:alert] = 'My birthday is soon.'
   end
 
 end
